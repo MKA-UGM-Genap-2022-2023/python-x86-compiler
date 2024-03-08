4906b8392741836fc0a307d72c58437edb2ced7e
Aranggi J. Toar <at@aranggitoar.com>
2024-03-08 20:11:20 +0700

    Logic: Basic argument, file, and string handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!();
+    let args: &Vec<String> = &env::args().collect::<Vec<String>>();
+    let contents = fs::read_to_string(&args[1]).expect("File should be read fine.");
+    println!("File contents: {}", contents);
+
+    let lines = contents.lines();
+
+    for line in lines {
+        println!("\nLine: {}\n", line);
+        let tokens = line.split(" ");
+        for token in tokens {
+            println!("{}", token);
+        }
+    }
 }
